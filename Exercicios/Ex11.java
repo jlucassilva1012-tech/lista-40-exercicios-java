53b4d609c972e302a6f8b83c72d538e4930805e0
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:43:17 -0300

    Implement multiplication table skipping multiples of 3
    Add a loop to multiply a user-input number by 1 to 10, skipping multiples of 3.

--- Exercicios/Ex11.java
+++ Exercicios/Ex11.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class Ex11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 3 != 0) {
+                System.out.println(n + " x " + i + " = " + n * i);
+            }
+        }
+    }
+}
