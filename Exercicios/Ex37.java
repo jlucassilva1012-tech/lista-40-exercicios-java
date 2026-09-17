122f1ce7c8695e597fc534ead7637f090b05d3a6
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:48:21 -0300

    Implement customer service simulation in Ex37

--- Exercicios/Ex37.java
+++ Exercicios/Ex37.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Ex37 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantidade de clientes: ");
+        int n = sc.nextInt();
+
+        int cliente = 1;
+
+        while (cliente <= n) {
+            System.out.println("Atendendo cliente " + cliente);
+
+            if (cliente % 5 == 0) {
+                System.out.println("5º cliente atendido!");
+            }
+
+            cliente++;
+        }
+    }
+}
