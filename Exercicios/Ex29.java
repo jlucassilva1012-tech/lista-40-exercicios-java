1e9b8ddf605510cfa88892e1770ae28beda2b744
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:46:48 -0300

    Implement leap year checker in Ex29.java
    Add a program to print leap years from 2000 to 2030.

--- Exercicios/Ex29.java
+++ Exercicios/Ex29.java
@@ -1,1 +1,10 @@
+public class Ex29 {
+    public static void main(String[] args) {
 
+        for (int ano = 2000; ano <= 2030; ano++) {
+            if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) {
+                System.out.println(ano);
+            }
+        }
+    }
+}
