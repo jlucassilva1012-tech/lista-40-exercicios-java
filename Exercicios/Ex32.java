67402de538db2ae24a5d8c3faa46aefd52b6d305
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:47:25 -0300

    Implement pyramid pattern printing based on user input
    This program prompts the user for a number N and prints a pyramid pattern of asterisks based on the input.

--- Exercicios/Ex32.java
+++ Exercicios/Ex32.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Ex32 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite N: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                if (j <= n - i) {
+                    System.out.print(" ");
+                }
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
