dc020eb252d0f0736daa427379d7af5589303763
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:47:53 -0300

    Update Ex34.java

--- Exercicios/Ex34.java
+++ Exercicios/Ex34.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Ex34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Primeiro número: ");
+        int a = sc.nextInt();
+
+        System.out.print("Segundo número: ");
+        int b = sc.nextInt();
+
+        while (b != 0) {
+            int resto = a % b;
+            a = b;
+            b = resto;
+        }
+
+        System.out.println("MDC: " + a);
+    }
+}
