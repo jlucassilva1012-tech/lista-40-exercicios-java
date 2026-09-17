788bc07c6c17a84b9ffe5293a79e914e13952d18
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:48:44 -0300

    Implement cumulative sum calculation in Ex39
    Add a program to calculate and print the cumulative sum up to a limit.

--- Exercicios/Ex39.java
+++ Exercicios/Ex39.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Ex39 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o limite: ");
+        int n = sc.nextInt();
+
+        int soma = 0;
+
+        for (int i = 1; soma <= n; i++) {
+            soma = soma + i;
+
+            if (soma <= n) {
+                System.out.println(soma);
+            }
+        }
+    }
+}
