4b5b3d8446db89e4d47a5c10ea6eae15e0852177
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:44:54 -0300

    Implement Collatz conjecture in Ex20.java

--- Exercicios/Ex20.java
+++ Exercicios/Ex20.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Ex20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite N: ");
+        int n = sc.nextInt();
+
+        int passos = 0;
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n * 3 + 1;
+            }
+
+            passos++;
+            System.out.println(n);
+        }
+
+        System.out.println("Passos: " + passos);
+    }
+}
