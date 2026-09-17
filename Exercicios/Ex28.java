7bdf5a9f2986d0eb425541a939eed02df8226828
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:46:39 -0300

    Update Ex28.java

--- Exercicios/Ex28.java
+++ Exercicios/Ex28.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Ex28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Valor inicial: ");
+        double valor = sc.nextDouble();
+
+        System.out.print("Taxa: ");
+        double taxa = sc.nextDouble();
+
+        System.out.print("Limite: ");
+        double limite = sc.nextDouble();
+
+        for (int mes = 1; mes <= 12; mes++) {
+            valor = valor + (valor * taxa / 100);
+
+            System.out.println("Mês " + mes + ": " + valor);
+
+            if (valor > limite) {
+                System.out.println("Ultrapassou o limite!");
+            }
+        }
+    }
+}
