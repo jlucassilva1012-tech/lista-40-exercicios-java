52317d32649f1e33b9c61209a1591cdf0ad0a0ad
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:42:28 -0300

    Implement perfect number finder in Ex07.java
    Add a program to print perfect numbers up to 500.

--- Exercicios/Ex07.java
+++ Exercicios/Ex07.java
@@ -1,1 +1,18 @@
+public class Ex07 {
+    public static void main(String[] args) {
 
+        for (int i = 1; i <= 500; i++) {
+            int soma = 0;
+
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    soma = soma + j;
+                }
+            }
+
+            if (soma == i) {
+                System.out.println(i);
+            }
+        }
+    }
+}
