fca425b7a6c1094194d9906f687da943678e609e
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:46:25 -0300

    Implement number guessing game in Ex27.java

--- Exercicios/Ex27.java
+++ Exercicios/Ex27.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Ex27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int secreto = 7;
+        int chute;
+
+        do {
+            System.out.print("Digite seu chute: ");
+            chute = sc.nextInt();
+
+            if (chute > secreto) {
+                System.out.println("O número é menor.");
+            } else if (chute < secreto) {
+                System.out.println("O número é maior.");
+            } else {
+                System.out.println("Acertou!");
+            }
+
+        } while (chute != secreto);
+    }
+}
