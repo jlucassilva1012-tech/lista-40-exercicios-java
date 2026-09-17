1dda0d3e1c622d2ced8bbfac61c415c88c7e28a7
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:43:37 -0300

    Update Ex13.java

--- Exercicios/Ex13.java
+++ Exercicios/Ex13.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Ex13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos alunos? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Digite a nota: ");
+            double nota = sc.nextDouble();
+
+            if (nota >= 7) {
+                System.out.println("Aprovado");
+            } else if (nota >= 5) {
+                System.out.println("Recuperação");
+            } else {
+                System.out.println("Reprovado");
+            }
+        }
+    }
+}
