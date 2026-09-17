8594a4a866fa036a186a6c1c2715a2969c1a38d7
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:45:03 -0300

    Update Ex21.java

--- Exercicios/Ex21.java
+++ Exercicios/Ex21.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Ex21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma frase: ");
+        String frase = sc.nextLine();
+
+        int vogais = 0;
+
+        for (int i = 0; i < frase.length(); i++) {
+            char letra = frase.charAt(i);
+
+            if (letra == 'a' || letra == 'e' || letra == 'i' ||
+                letra == 'o' || letra == 'u') {
+                vogais++;
+            }
+        }
+
+        System.out.println("Vogais: " + vogais);
+    }
+}
