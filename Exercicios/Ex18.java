8b02d2d9afb094e13509ab706f300e063d164fb8
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:44:33 -0300

    Implement number digit sum calculation

--- Exercicios/Ex18.java
+++ Exercicios/Ex18.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Ex18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        int soma = 0;
+
+        while (numero != 0) {
+            soma = soma + numero % 10;
+            numero = numero / 10;
+        }
+
+        System.out.println("Soma: " + soma);
+    }
+}
