376c0e806f0a139096c614ae838320495c668457
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:42:12 -0300

    Implement factorial calculation in Ex06.java
    Add a program to calculate the factorial of a number.

--- Exercicios/Ex06.java
+++ Exercicios/Ex06.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Ex06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite N: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Número negativo!");
+        } else {
+            int fatorial = 1;
+
+            for (int i = 1; i <= n; i++) {
+                fatorial = fatorial * i;
+            }
+
+            System.out.println("Fatorial: " + fatorial);
+        }
+    }
+}
