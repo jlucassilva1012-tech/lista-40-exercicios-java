4e3c40f93f7cad73c5def6fffd7acf8f74a9b20b
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:45:37 -0300

    Update Ex24.java

--- Exercicios/Ex24.java
+++ Exercicios/Ex24.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Ex24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        while (i <= 10) {
+            int resultado = n * i;
+
+            System.out.println(resultado);
+
+            if (resultado % 7 == 0) {
+                break;
+            }
+
+            i++;
+        }
+    }
+}
