4b36d4d4235a245465413a652171b76d75a928e8
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:43:08 -0300

    Add ATM withdrawal functionality in Ex10.java
    Implement a simple ATM withdrawal system that checks for sufficient balance.

--- Exercicios/Ex10.java
+++ Exercicios/Ex10.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Ex10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double saldo = 1000;
+        double saque;
+        int opcao;
+
+        do {
+            System.out.print("Digite o valor do saque: ");
+            saque = sc.nextDouble();
+
+            if (saque <= saldo) {
+                saldo = saldo - saque;
+                System.out.println("Saque realizado!");
+            } else {
+                System.out.println("Saldo insuficiente!");
+            }
+
+            System.out.println("Saldo: " + saldo);
+            System.out.print("Digite 1 para continuar ou 0 para sair: ");
+            opcao = sc.nextInt();
+
+        } while (opcao != 0);
+    }
+}
