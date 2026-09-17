2df42660af9f187484cd786ef76f9372f3f20737
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:43:48 -0300

    Add student approval/rejection counting program

--- Exercicios/Ex14.java
+++ Exercicios/Ex14.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class Ex14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos alunos? ");
+        int n = sc.nextInt();
+
+        int aprovados = 0;
+        int reprovados = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Digite a média: ");
+            double media = sc.nextDouble();
+
+            if (media >= 7) {
+                aprovados++;
+            } else {
+                reprovados++;
+            }
+        }
+
+        System.out.println("Aprovados: " + aprovados);
+        System.out.println("Reprovados: " + reprovados);
+    }
+}
