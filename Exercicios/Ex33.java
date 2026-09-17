5c2b7ba0318738777a60dbf21f42b09eb9288a31
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:47:44 -0300

    Update Ex33.java

--- Exercicios/Ex33.java
+++ Exercicios/Ex33.java
@@ -1,1 +1,16 @@
+public class Ex33 {
+    public static void main(String[] args) {
 
+        for (int celsius = 0; celsius <= 100; celsius++) {
+            double fahrenheit = celsius * 9.0 / 5 + 32;
+
+            if (celsius == 0) {
+                System.out.println(celsius + "°C = " + fahrenheit + "°F - Congelamento");
+            } else if (celsius == 100) {
+                System.out.println(celsius + "°C = " + fahrenheit + "°F - Ebulição");
+            } else {
+                System.out.println(celsius + "°C = " + fahrenheit + "°F");
+            }
+        }
+    }
+}
