13c14b12e040ba31891b404421c23ae30b6d82f5
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:43:28 -0300

    Implement number comparison program
    This program prompts the user to input a specified number of integers and determines the largest and smallest numbers among them.

--- Exercicios/Ex12.java
+++ Exercicios/Ex12.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+public class Ex12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos números? ");
+        int n = sc.nextInt();
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        int maior = numero;
+        int menor = numero;
+
+        for (int i = 2; i <= n; i++) {
+            System.out.print("Digite um número: ");
+            numero = sc.nextInt();
+
+            if (numero > maior) {
+                maior = numero;
+            }
+
+            if (numero < menor) {
+                menor = numero;
+            }
+        }
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+    }
+}
