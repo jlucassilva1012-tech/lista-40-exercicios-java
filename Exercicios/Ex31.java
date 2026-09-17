c2a4ae316ddcf8a8539d9c164653334134ab8165
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:47:15 -0300

    Update Ex31.java

--- Exercicios/Ex31.java
+++ Exercicios/Ex31.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+public class Ex31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos números? ");
+        int n = sc.nextInt();
+
+        int positivos = 0;
+        int negativos = 0;
+        int zeros = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Digite um número: ");
+            int numero = sc.nextInt();
+
+            if (numero > 0) {
+                positivos++;
+            } else if (numero < 0) {
+                negativos++;
+            } else {
+                zeros++;
+            }
+        }
+
+        System.out.println("Positivos: " + positivos);
+        System.out.println("Negativos: " + negativos);
+        System.out.println("Zeros: " + zeros);
+    }
+}
