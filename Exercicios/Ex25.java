5ba4ecdd85dc7caaa6ed28742a8624db08b08a42
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:45:56 -0300

    Update Ex25.java

--- Exercicios/Ex25.java
+++ Exercicios/Ex25.java
@@ -1,1 +1,12 @@
+public class Ex25 {
+    public static void main(String[] args) {
 
+        for (int i = 1; i <= 30; i++) {
+            if (i % 4 == 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
