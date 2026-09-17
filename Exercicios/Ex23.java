fcdf647fcb1a512d263dca666aeb66bdafd73838
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:45:28 -0300

    Update Ex23.java

--- Exercicios/Ex23.java
+++ Exercicios/Ex23.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Ex23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma palavra: ");
+        String palavra = sc.nextLine();
+
+        boolean palindromo = true;
+
+        for (int i = 0; i < palavra.length() / 2; i++) {
+            if (palavra.charAt(i) != palavra.charAt(palavra.length() - 1 - i)) {
+                palindromo = false;
+            }
+        }
+
+        if (palindromo) {
+            System.out.println("É palíndroma");
+        } else {
+            System.out.println("Não é palíndroma");
+        }
+    }
+}
