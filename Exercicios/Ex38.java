ae04158fdc50ce229e83f712ef7566ae0a47240f
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:48:29 -0300

    Update Ex38.java

--- Exercicios/Ex38.java
+++ Exercicios/Ex38.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Ex38 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a base: ");
+        int base = sc.nextInt();
+
+        System.out.print("Digite o expoente: ");
+        int expoente = sc.nextInt();
+
+        int resultado = 1;
+
+        if (expoente == 0) {
+            resultado = 1;
+        } else {
+            for (int i = 1; i <= expoente; i++) {
+                resultado = resultado * base;
+            }
+        }
+
+        System.out.println("Resultado: " + resultado);
+    }
+}
