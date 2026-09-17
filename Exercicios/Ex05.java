b37f49a70cf3a92f5f11846bf1e03fd30498c536
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:38:43 -0300

    Add Java program to count prime numbers up to 50

--- Exercicios/Ex05.java
+++ Exercicios/Ex05.java
@@ -1,1 +1,22 @@
 
+public class Ex05 {
+    public static void main(String[] args) {
+        int quantidade = 0;
+
+        for (int i = 2; i <= 50; i++) {
+            int divisores = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    divisores++;
+                }
+            }
+
+            if (divisores == 2) {
+                quantidade++;
+            }
+        }
+
+        System.out.println("Quantidade de primos: " + quantidade);
+    }
+}
