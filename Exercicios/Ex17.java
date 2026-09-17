fa12d337071e229272e8ce23ddb1dec50368dd73
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:44:24 -0300

    Count digits in a number input by the user

--- Exercicios/Ex17.java
+++ Exercicios/Ex17.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Ex17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        int quantidade = 0;
+
+        if (numero == 0) {
+            quantidade = 1;
+        }
+
+        while (numero != 0) {
+            numero = numero / 10;
+            quantidade++;
+        }
+
+        System.out.println("Quantidade de dígitos: " + quantidade);
+    }
+}
