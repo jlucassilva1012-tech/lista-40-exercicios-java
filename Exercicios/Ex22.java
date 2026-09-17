2efd057c3b9b764cbdd369f6e28755bcd0f9e8b8
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:45:20 -0300

    Add letter case counting functionality
    This program counts the number of uppercase and lowercase letters in a given phrase.

--- Exercicios/Ex22.java
+++ Exercicios/Ex22.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Ex22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma frase: ");
+        String texto = sc.nextLine();
+
+        int maiusculas = 0;
+        int minusculas = 0;
+
+        for (int i = 0; i < texto.length(); i++) {
+            char letra = texto.charAt(i);
+
+            if (letra >= 'A' && letra <= 'Z') {
+                maiusculas++;
+            } else if (letra >= 'a' && letra <= 'z') {
+                minusculas++;
+            }
+        }
+
+        System.out.println("Maiúsculas: " + maiusculas);
+        System.out.println("Minúsculas: " + minusculas);
+    }
+}
