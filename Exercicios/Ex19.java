38eefb680c6723b07a0403d8e19cf2e21eebed69
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:44:46 -0300

    Update Ex19.java

--- Exercicios/Ex19.java
+++ Exercicios/Ex19.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Ex19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        int invertido = 0;
+
+        while (numero != 0) {
+            int resto = numero % 10;
+            invertido = invertido * 10 + resto;
+            numero = numero / 10;
+        }
+
+        System.out.println("Invertido: " + invertido);
+    }
+}
