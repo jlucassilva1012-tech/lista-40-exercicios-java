fe3de25c3125665b81d9eb8f13b60556c6d070bf
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:48:11 -0300

    Implement number sequence check for increasing order

--- Exercicios/Ex36.java
+++ Exercicios/Ex36.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+public class Ex36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int anterior = 0;
+        boolean crescente = true;
+        boolean primeiro = true;
+
+        while (true) {
+            System.out.print("Digite um número (-1 para parar): ");
+            int numero = sc.nextInt();
+
+            if (numero == -1) {
+                break;
+            }
+
+            if (!primeiro) {
+                if (numero <= anterior) {
+                    crescente = false;
+                }
+            }
+
+            anterior = numero;
+            primeiro = false;
+        }
+
+        if (crescente) {
+            System.out.println("A sequência é crescente.");
+        } else {
+            System.out.println("A sequência não é crescente.");
+        }
+    }
+}
