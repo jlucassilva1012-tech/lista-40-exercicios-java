a9e6455f282c9da15c0d7250e024241e2afdecd7
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:42:53 -0300

    Implement countdown program with user input
    This program takes an integer input N and counts down to 1, printing each number, followed by a message 'Decolagem!'

--- Exercicios/Ex09.java
+++ Exercicios/Ex09.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Ex09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite N: ");
+        int n = sc.nextInt();
+
+        while (n >= 1) {
+            System.out.println(n);
+            n--;
+        }
+
+        System.out.println("Decolagem!");
+    }
+}
