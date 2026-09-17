69e52e9a976743a6c049990942e5af311e51d9cf
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:42:41 -0300

    Implement Fibonacci sequence with even/odd output
    Prints the first 20 Fibonacci numbers with parity indication.

--- Exercicios/Ex08.java
+++ Exercicios/Ex08.java
@@ -1,1 +1,20 @@
+public class Ex08 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
 
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(a);
+
+            if (a % 2 == 0) {
+                System.out.println(" - Par");
+            } else {
+                System.out.println(" - Ímpar");
+            }
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
