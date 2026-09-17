c24124d6ff5a6ba8eec2d327264d3d8ebb803530
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:48:02 -0300

    Update Ex35.java

--- Exercicios/Ex35.java
+++ Exercicios/Ex35.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Ex35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Primeiro número: ");
+        int a = sc.nextInt();
+
+        System.out.print("Segundo número: ");
+        int b = sc.nextInt();
+
+        int maior;
+
+        if (a > b) {
+            maior = a;
+        } else {
+            maior = b;
+        }
+
+        int mmc = maior;
+
+        while (mmc % a != 0 || mmc % b != 0) {
+            mmc++;
+        }
+
+        System.out.println("MMC: " + mmc);
+    }
+}
