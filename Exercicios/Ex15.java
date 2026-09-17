0b8ba246c73cd901899bf597064aacb46088f799
jlucassilva1012-tech <jlucassilva1012@gmail.com>
2026-09-16 21:43:59 -0300

    Implement palindrome check in Ex15.java
    Added functionality to check if a number is a palindrome.

--- Exercicios/Ex15.java
+++ Exercicios/Ex15.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Ex15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        int original = numero;
+        int invertido = 0;
+
+        while (numero != 0) {
+            int resto = numero % 10;
+            invertido = invertido * 10 + resto;
+            numero = numero / 10;
+        }
+
+        if (original == invertido) {
+            System.out.println("É palíndromo");
+        } else {
+            System.out.println("Não é palíndromo");
+        }
+    }
+}
